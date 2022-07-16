c00d3ccdf748d89d2b5a336657738cfa68154eb7
Elia Martinez <eliahaydeemartinez@gmail.com>
2022-07-16 10:31:40 -0500

    changed catname

--- seeds/seed.js
+++ seeds/seed.js
@@ -5,7 +5,7 @@ const SkillCategory = require('../models/SkillCategory');
 const ServiceRequest = require('../models/ServiceRequest');
 
 const userData = require('./userData.json');
-const categoryData = require('./categoryData.json');
+const categoryData = require('./catData.json');
 const skillData = require('./skillData.json');
 const skillCategoryData = require('./skillCategoryData.json');
 const userSkillData = require('./userSkillData.json');
