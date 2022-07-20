92063402d17658f263cba7945f67b623faa2b794
Anthony Poku <tonypoku@gmail.com>
2022-07-20 01:55:46 -0500

    worked on dashboard flow

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,4 +1,8 @@
-const gotoGettingStarted = (btn) => {
+const gotoGettingStarted = () => {
+  document.location.replace('/api/services/getting-started');
+};
+
+const gotoPublishSkill = (e) => {
   document.location.replace('/api/services/getting-started');
 };
 
@@ -6,4 +10,16 @@ if (document.querySelector('#continue_getting_started_btn')) {
   document
     .querySelector('#continue_getting_started_btn')
     .addEventListener('click', gotoGettingStarted);
+}
+
+if (document.querySelector('#edit_service_commitment')) {
+  document
+    .querySelector('#edit_service_commitment')
+    .addEventListener('click', editServiceCommitmentHandler);
+}
+
+if (document.querySelector('#update_existing_skill_btn')) {
+  document
+    .querySelector('#update_existing_skill_btn')
+    .addEventListener('click', gotoPublishSkill);
 }
